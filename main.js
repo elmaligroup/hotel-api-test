8611fc6d7c9a2212bb9a445a66a155dac9cc0f02
berkay8uhlar <2003005032@student.beykent.edu.tr>
2023-01-04 18:00:24 +0300

    access

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 
-window.onload = () => {
-    axios({
-        method : 'get',
-        url : `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
-        params : {
-            keyword : "hotel",
-            location : "-.33.8670522%2C151.1957362",
-            radius : "1500",
-            type : "lodging",
-            key : 'AIzaSyAdukdB6pwDKHEgqaGghV7TfqwtbDEfJRo'
-        },
-        headers : {
-            origin : "https://hotel-api-test.vercel.app"
-        }
-    })
-    .then(response => {console.log(response)})
+// window.onload = () => {
+//     // axios({
+//     //     method : 'get',
+//     //     url : 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyAdukdB6pwDKHEgqaGghV7TfqwtbDEfJRo',
+//     //     // url : `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+//     //     // params : {
+//     //     //     keyword : "hotel",
+//     //     //     location : "-.33.8670522%2C151.1957362",
+//     //     //     radius : "1500",
+//     //     //     type : "lodging",
+//     //     //     key : 'AIzaSyAdukdB6pwDKHEgqaGghV7TfqwtbDEfJRos'
+//     //     // },
 
-}
+//     // })
+//     // .then(response => {console.log(response)})
+//     axios.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJFfyzTTeuEmsRuMxvFyNRfbk&key=AIzaSyAdukdB6pwDKHEgqaGghV7TfqwtbDEfJRo')
+//     .then(response => {
+//         console.log(response)
+//     })
+// }
