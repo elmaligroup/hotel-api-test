925b774623a0e90f05cdac09fdf44e40b54306d8
berkay8uhlar <2003005032@student.beykent.edu.tr>
2023-01-04 17:06:07 +0300

    files

---
+++ main.js
@@ -0,0 +1,16 @@
+
+window.onload = () => {
+    axios({
+        method : 'get',
+        url : `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+        params : {
+            keyword : "hotel",
+            location : "-.33.8670522%2C151.1957362",
+            radius : "1500",
+            type : "lodging",
+            key : 'AIzaSyAdukdB6pwDKHEgqaGghV7TfqwtbDEfJRo'
+        }
+    })
+    .then(response => {console.log(response)})
+
+}
